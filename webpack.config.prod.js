e88e6c09c98855e2ff5766c59c949f696924b740
Aarón Blasco <arionblasco@gmail.com>
2024-11-26 03:05:26 +0100

    Some config changes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,9 +2,26 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // Match .ts files
+        use: 'ts-loader',
+        exclude: /node_modules/, // Exclude dependencies
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
